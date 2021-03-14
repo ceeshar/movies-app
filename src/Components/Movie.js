d3a9a134db6302823e3506a89b3ab2110221bc25
Ceeshar Cee <ibrahimuzuum@gmail.com>
2021-03-14 20:28:52 +0300

    14.03

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -4,7 +4,17 @@ import { IMG_API } from '../variables';
 const Movie = ({ title, poster_path, overview, vote_average }) => (
   <div className='movie'>
     <img src={IMG_API + poster_path} alt={title} />
+    <div className="movie-info">
+      <h3>{title}</h3>
+      <span>{vote_average}</span>
+    </div>
+        <div className="movie-over">
+          <h2>Overview:</h2>
+          <p>{overview}</p>
+        </div>
+
   </div>
+
 );
 
 export default Movie;
